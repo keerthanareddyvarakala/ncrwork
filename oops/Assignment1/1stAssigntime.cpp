be92ba4950c00463957c28efc8e534525dcd9e94
keerthanareddyvarakala <keerthanavarakala@gmail.com>
2019-02-13 12:17:26 +0530

    added time program

---
+++ oops/Assignment1/1stAssigntime.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+class time
+{
+	int hours;
+	int minutes;
+	int seconds;
+public:
+	time()
+	{
+		hours = minutes = seconds = 0;
+	}
+	time(int h, int m, int s)
+	{
+		hours = h;
+		minutes = m;
+		seconds = s;
+	}
+	void display()
+	{
+		cout << hours << ":" << minutes << ":" << seconds << endl;
+	}
+	void add_time(time &t1, time &t2)
+	{
+		hours = t1.hours + t2.hours;
+		minutes = t1.minutes + t2.minutes;
+		if (minutes > 60)
+		{
+			hours++;
+			minutes =  minutes -60;
+		}
+		seconds = t1.seconds + t2.seconds;
+		if (seconds > 60)
+		{
+			minutes++;
+			seconds = seconds-60;
+		}
+
+	}
+};
+void main()
+{
+	time t1(10, 10, 40), t2(20, 20, 30), t3;
+	t1.display();
+	t2.display();
+	t3.add_time(t1, t2);
+	t3.display();
+
+}
