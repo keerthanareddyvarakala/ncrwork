d6d5371031fc754d6af5fa6fbf8b90f92430ea90
keerthanareddyvarakala <keerthanavarakala@gmail.com>
2019-02-19 17:00:39 +0530

    oops assignment

---
+++ oops/Assignment2/3emp.cpp
@@ -0,0 +1,78 @@
+<<<<<<< HEAD
+#include <iostream>
+#include<string>
+using namespace std;
+class EMPLOYEE
+{
+public:
+	int empno;
+	char name[20];
+	float salary;
+	void display()
+	{
+		cout << "The details of the employee are" << endl;
+		cout << "empno=" << empno << endl;
+		cout << "name=" << name << endl;
+		cout << "salary=" << salary << endl;
+	}
+	friend istream operator&(istream& cin, EMPLOYEE& e);
+};
+istream& operator >> (istream& cin, EMPLOYEE& e)
+{
+	cin >> e.empno;
+	cin >> e.name;
+	cin >> e.salary;
+	return cin;
+}
+int main()
+{
+	EMPLOYEE e[2];
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "enter the values of" << i << "th object" << endl;
+		cin >> e[i];
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		e[i].display();
+	}
+=======
+#include <iostream>
+#include<string>
+using namespace std;
+class EMPLOYEE
+{
+public:
+	int empno;
+	char name[20];
+	float salary;
+	void display()
+	{
+		cout << "The details of the employee are" << endl;
+		cout << "empno=" << empno << endl;
+		cout << "name=" << name << endl;
+		cout << "salary=" << salary << endl;
+	}
+	friend istream operator&(istream& cin, EMPLOYEE& e);
+};
+istream& operator >> (istream& cin, EMPLOYEE& e)
+{
+	cin >> e.empno;
+	cin >> e.name;
+	cin >> e.salary;
+	return cin;
+}
+int main()
+{
+	EMPLOYEE e[2];
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "enter the values of" << i << "th object" << endl;
+		cin >> e[i];
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		e[i].display();
+	}
+>>>>>>> 4dfe742b3a34258499ebc2f3017dc670aceac702
+}
